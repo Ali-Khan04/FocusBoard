023a2596dbd63b91235a7ace8cfea0706763f9a0
Ali-Khan04 <aliiiiahmaaeed@gmail.com>
2025-08-06 12:11:06 +0500

    Refactor component to use useState for user data

--- userInput.jsx
+++ userInput.jsx
@@ -1,13 +1,27 @@
+import { useState } from "react";
 function UserInput() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [date, setDate] = useState("");
+  const handleTitle = (event) => {
+    setTitle(event.target.value);
+  };
+  const handleDescription = (event) => {
+    setDescription(event.target.value);
+  };
+  const handleDate = (event) => {
+    setDate(event.target.value);
+  };
   return (
     <div className="input container">
       <form>
         <label>Title</label>
-        <input type="text" />
+        <input type="text" value={title} onChange={handleTitle} />
         <label>Description</label>
-        <input type="text" />
+        <input type="text" value={description} onChange={handleDescription} />
         <label>Date</label>
-        <input type="Date" />
+        <input type="Date" value={date} onChange={handleDate} />
+        <button type="submit">Add</button>
       </form>
     </div>
   );
