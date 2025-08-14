05bc85a2849a072a95e0bfbbebff91a58279a643
Ali-Khan04 <aliiiiahmaaeed@gmail.com>
2025-08-14 11:41:04 +0500

    add signIn/signUp pages with styling

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,7 +1,13 @@
+import "../CSS/signUp.css";
 function SignIn() {
   return (
-    <div>
+    <div className="sign-in-container">
       <h1>Sign in</h1>
+      <form>
+        <input type="email" required placeholder="Email" />
+        <input type="password" required placeholder="password" />
+        <button>Sign In</button>
+      </form>
     </div>
   );
 }
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,7 +1,14 @@
+import "../CSS/signUp.css";
 function SignUp() {
   return (
-    <div>
+    <div className="sign-up-container">
       <h1>SignUp</h1>
+      <form>
+        <input type="text" required placeholder="Name" />
+        <input type="email" required placeholder="Email" />
+        <input type="password" required placeholder="password" />
+        <button>SignUp</button>
+      </form>
     </div>
   );
 }
